c621a7a1752716265fe4086e43879ec905a0b56e
psoerens <p.soerens@outlook.com>
2018-02-07 17:12:36 -0800

    Brute Force 1
    Initial setup of Bruteforce method

---
+++ Euler3.java
@@ -0,0 +1,23 @@
+/**
+ * Working solution on project Euler problem 3: Largest Prime Factor
+ * https://projecteuler.net/problem=3
+ * 
+ * CUrrently WIP, this is currently factor finding
+ */
+
+
+public class Euler3 {
+	
+	public Euler3(int N){
+		
+		int factors[] = new int[20]; //arbitrary number of factors, will need potential scaling method
+	}
+
+	public static void main(String[] args)
+	{
+		int N = 0;	//determine the factors of N
+		
+		new Euler3(N);
+	}
+
+}
